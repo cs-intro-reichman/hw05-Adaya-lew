fc62cce892c938bab52c3d627008f1233327721a
Adaya-lew <adaya.lewartowski@post.runi.ac.il>
2024-01-22 11:22:42 +0200

    hw05

--- GameOfLife.java
+++ GameOfLife.java
@@ -168,7 +168,7 @@
     public static void print(int[][] arr) {
     	for(int i=0; i< arr.length-1; i++){
     		for(int j=0; j< arr[i].length-1; j++){
-    			System.out.printf("%3s" + arr[i][j]);
+    			System.out.printf(" " + arr[i][j]);
     		}
 			System.out.println();
     	}
